75a5fed1180a9a18e64ea9deb534c13ce40c5501
brainwu <brainwu1215@gmail.com>
2014-09-08 21:38:24 +0800

    add getarticle looger

--- controllers/blog/index.go
+++ controllers/blog/index.go
@@ -1,8 +1,10 @@
 package blog
 
 import (
-	"github.com/brainwu/brainblog/models"
 	"strings"
+
+	"github.com/brainwu/brainblog/models"
+	beego "gopkg.in/astaxie/beego.v1"
 )
 
 type IndexController struct {
@@ -19,20 +21,21 @@ func (this *IndexController) Get() {
 func (this *IndexController) GetArticle() {
 	sPath := this.Ctx.Request.URL.Path
 	arr := strings.Split(sPath, "/")
-	if (len(arr) <= 0) {
+	if len(arr) <= 0 {
 		this.Redirect("/", 302)
 		return
 	}
 	sTitle := arr[len(arr)-1]
 	var a *models.Article = &models.Article{
-		Title:sTitle,
+		Title: sTitle,
 	}
 	if err := a.Read("Title"); err != nil {
+		beego.Error("GetArticle read " + sTitle + " : " + err.Error())
 		this.Redirect("/", 302)
 		return
 	} else {
 		this.Data["title"] = a.Title
 		this.Data["content"] = a.Content
-		this.TplNames="article.html"
+		this.TplNames = "article.html"
 	}
 }
